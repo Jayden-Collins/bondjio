c69a423cd78e1cd56bd9345d577e93e368eeeb4c
Tan Yue Heng <tanyh-pm24@student.tarc.edu.my>
2025-08-07 23:14:49 +0800

    Add Screen Files (#17)
    * add icons
    
    * add brand color
    
    * add bottomNavBar
    
    * add topBar
    
    * add font
    
    * add font
    
    * add font files
    
    * add launcher icon
    
    * add google-services.json
    
    * add screen files

---
+++ app/src/main/java/com/example/bondjio/ui/screens/Login.kt
@@ -0,0 +1,26 @@
+package com.example.bondjio.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.bondjio.ui.navigation.BottomNavBar
+import com.example.bondjio.ui.navigation.TopBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(navController: NavController) {
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomNavBar(navController) },
+        content = { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                Text("Search Filters")
+            }
+        }
+    )
+}
