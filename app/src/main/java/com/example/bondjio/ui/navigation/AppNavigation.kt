01826e19a0df335dc859cd1f3583ca758db959e6
Tan Yue Heng <tanyh-pm24@student.tarc.edu.my>
2025-08-07 23:19:35 +0800

    add screen files

--- app/src/main/java/com/example/bondjio/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/bondjio/ui/navigation/AppNavigation.kt
@@ -9,11 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.bondjio.ui.screens.Community
+import com.example.bondjio.ui.screens.EventDetail
 import com.example.bondjio.ui.screens.Home
 import com.example.bondjio.ui.screens.Login
 import com.example.bondjio.ui.screens.Search
 import com.example.bondjio.ui.screens.Tickets
-import com.example.bondjio.ui.screens.Community
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,6 +43,11 @@ fun AppNavigation() {
 
         composable(Routes.Account.route) {
             Login(navController)
+        }
+
+        composable(Routes.EventDetail.route) {
+            // Pass the eventId?
+            EventDetail(navController)
         }
     }
 }
